0ff06c4fc5cb1fd309be06eed4d386bf79a133c7
leonovde <dmitrii@snappymob.com>
2019-09-22 23:15:04 +0800

    Kotlin: Convert about screen to kotlin

---
+++ app/src/main/java/com/leonov_dev/currencyconverter/ui/about/AboutFragment.kt
@@ -0,0 +1,16 @@
+package com.leonov_dev.currencyconverter.ui.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.leonov_dev.currencyconverter.R
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+}
