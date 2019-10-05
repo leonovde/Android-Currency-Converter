ece229a13316e5836db3fc7b91c969a0404bc801
leonovde <xormeradek@gmail.com>
2019-10-05 23:37:31 +0800

    API: Provide cache interceptor for http client

--- app/src/main/java/com/leonov_dev/currencyconverter/data/source/remote/ClientGenerator.kt
+++ app/src/main/java/com/leonov_dev/currencyconverter/data/source/remote/ClientGenerator.kt
@@ -2,12 +2,12 @@ package com.leonov_dev.currencyconverter.data.source.remote
 
 import android.app.Application
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import okhttp3.Cache
-import okhttp3.OkHttpClient
+import okhttp3.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
 import java.lang.Exception
+import java.util.concurrent.TimeUnit
 
 object ClientGenerator {
 
@@ -15,6 +15,7 @@ object ClientGenerator {
 
     private val okHttpClient = OkHttpClient
             .Builder()
+            .addNetworkInterceptor(provideCacheInterceptor())
             .cache(provideCache())
             .build()
 
@@ -32,5 +33,16 @@ object ClientGenerator {
         Cache(cacheFile, cacheSize)
     } catch (exception: Exception) {
         null
+    }
+
+    private fun provideCacheInterceptor() = Interceptor { chain ->
+        val cacheControl = CacheControl.Builder()
+                .maxAge(5, TimeUnit.MINUTES)
+                .build()
+
+        val response = chain.proceed(chain.request())
+        response.newBuilder()
+                .header("CACHE-CONTROL", cacheControl.toString())
+                .build()
     }
 }
